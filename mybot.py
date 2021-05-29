dd08ae5a209a25d73784b32cd50684c2f10340ad
danil <danilsubath@yandex.ru>
2021-05-29 20:07:52 +0700

    Test commit1

--- mybot.py
+++ mybot.py
@@ -7,6 +7,10 @@ import discord
 from discord.ext import commands
 import img_handler as imhl
 import os,random
+import img_handler as imhl
+import fight as f
+
+
 
 intense = discord.Intents.default()
 intense.members = True
@@ -103,29 +107,54 @@ async def vc_ost(ctx):
             voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
             await voice_client.play(discord.FFmpegPCMAudio(executable="./sound/ffmpeg.exe", source="./sound/mk.mp3"))
 
-@bot.command(name="ffight")
+@bot.command(name="ost")
+async def vs_ost(ctx):
+    voice_client = discord.utils.get(bot.voice_clients, guild = ctx.guild)
+    await ctx.channel.send(f'MORTAL COMBAAT')
+    await voice_client.play(discord.FFmpegPCMAudio(source="./mp3/mk.mp3"))
+
+@bot.command(name="fight")
 @allowed_channel()
-async def ffight(ctx:commands.Context):
-    f1 = None
-    f2 = bot.user
+async def fight(ctx:commands.Context):
+    # Первый претендент
+    f1:discord.Member = None
+    # Второй претендент
+    f2:discord.Member = bot.user
+    # Voice-канал участника
     voice_channel = ctx.author.voice.channel
     if voice_channel:
         await vc_join(ctx)
+        # Список активных пользователей
         voice_members = voice_channel.members
+        # Фильтруем пользователей, оставляя только людей
         voice_members = [member for member in voice_members if member.bot == False]
-        if len(voice_members)>1:
-            f1, f2 =[voice_members.pop(random.randint(0, len(voice_members)-1)),voice_members.pop(random.randint(0, len(voice_members)-1))]
-        
+        # Отбираем претендентов
+        if len(voice_members) > 1:
+            # a,b = [1, 2]   => a = 1    b = 2
+            f1, f2 = [voice_members.pop(random.randint(0, len(voice_members)-1)), voice_members.pop(random.randint(0, len(voice_members)-1))]
         else:
             f1 = ctx.author
-            
-        await voice_channel.connect()
+        # СОЗДАТЬ VS_SCREEN
+        await imhl.vs_create_animated(f1.avatar_url, f2.avatar_url)
 
-        await imhl.vs_create(f1.avatar_url,f2.avatar_url,f1.name,f2.name)
-        await ctx.channel.send( file=discord.File(os.path.join("./img/result.png")))
-        await imhl.vs_create_animated(f1.avatar_url, f2.avatar_url,f1.name,f2.name)
-        await ctx.channel.send(file=discord.File(os.path.join("./img/result.gif")))
-        voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-        await voice_client.play(discord.FFmpegPCMAudio(executable="./sound/ffmpeg.exe", source="./sound/mk.mp3"))
+        embed = discord.Embed(
+            title = "Let's Mortal Kombat Begins!",
+            description = f'{f1.display_name} бьётся с {f2.display_name}',
+            colour = discord.Colour.dark_purple(),
+        )
+
+        message = await ctx.channel.send(embed = embed, file=discord.File(os.path.join("./img/result.gif")))
+        # ЗАПУСТИТЬ МУЗЫКУ
+        voice_client = discord.utils.get(bot.voice_clients, guild = ctx.guild)
+        # await voice_client.play(discord.FFmpegPCMAudio(source="./mp3/mk.mp3"))
+
+        await f.create_fighters(f1, f2, message)
+
+
+        await voice_client.stop()
+
+        
+    else: 
+        await ctx.channel.send("Зайдите в voice-канал пожажя")
 
 bot.run(os.environ["BOT_TOKEN"])
